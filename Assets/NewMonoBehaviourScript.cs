c645c6e512ae9fe1e6defec731a6f05de5d913c1
mixaste444-lang <mixaste444@gmail.com>
2026-08-03 16:53:42 +0300

    initial unity project

---
+++ Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 7f;
+
+    private Rigidbody rb;
+    private bool isGrounded = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(x, 0, z);
+        transform.Translate(move * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+}
