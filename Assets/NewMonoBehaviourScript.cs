9f6c8965f33d6e8072ecfb067f934ac2f018f2b6
mixaste444-lang <mixaste444@gmail.com>
2026-08-03 17:28:29 +0300

    initial unity project

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
-    public float jumpForce = 3f;
+    public float jumpForce = 7f;
 
     private Rigidbody rb;
     private bool isGrounded = true;
